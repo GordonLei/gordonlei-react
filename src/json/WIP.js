8a33065c51d7b725c25d0cc365d084db6ec37b91
Gordon Lei <gl1776@nyu.edu>
2021-09-15 02:04:03 -0400

    added neco-arc to WIP projects. no images

--- src/json/WIP.js
+++ src/json/WIP.js
@@ -1,22 +1,34 @@
 const WIP = [
-    {
-        "title": "Augmented Library",
-        "technology": ["Swift", "Python", "A*", "ARKit", "Firebase"],
-        "subheader": "Creating AR experiences for the library",
-        "description": "Augmented Library is a project team dedicated to making tools and services for NYU's Dibner Library. The goal of the team is to make applications that can incorporate Augmented Reality in order to enhance the library experience. The two main projects that we are currently working on include are \"Interactive Flyers\" and \"Path-Finding\". The goal of the Interactive Flyers project is to create scannable images similar to QR codes (but looks visually more appealing or related to the university) that will act as a billboard flyer. By scanning the image, the user will be prompted with an AR experience explaining the details of the flyer. The goal of the Path-Finding team is to use the A* algorithmn as a basis to create a tool that can be used by students to navigate to specific places within the library",
-        "demoLink": "",
-        "githubLink": "https://github.com/Augmented-Library/Augmented-Library",
-        "images": []
-    },
-    {
-        "title": "Portfolio",
-        "technology": ["Node.js", "React", "TailwindCSS"],
-        "subheader": "My personal website",
-        "description": "My personal website for my portfolio. Will be working on making the website look good for different viewports as well as color palettes!",
-        "demoLink": "",
-        "githubLink": "https://github.com/Augmented-Library/Augmented-Library",
-        "images": [process.env.PUBLIC_URL + '/img/website.png']
-    },
+  {
+    title: "Augmented Library",
+    technology: ["Swift", "Python", "A*", "ARKit", "Firebase"],
+    subheader: "Creating AR experiences for the library",
+    description:
+      'Augmented Library is a project team dedicated to making tools and services for NYU\'s Dibner Library. The goal of the team is to make applications that can incorporate Augmented Reality in order to enhance the library experience. The two main projects that we are currently working on include are "Interactive Flyers" and "Path-Finding". The goal of the Interactive Flyers project is to create scannable images similar to QR codes (but looks visually more appealing or related to the university) that will act as a billboard flyer. By scanning the image, the user will be prompted with an AR experience explaining the details of the flyer. The goal of the Path-Finding team is to use the A* algorithmn as a basis to create a tool that can be used by students to navigate to specific places within the library',
+    demoLink: "",
+    githubLink: "https://github.com/Augmented-Library/Augmented-Library",
+    images: [],
+  },
+  {
+    title: "Portfolio",
+    technology: ["Node.js", "React", "TailwindCSS"],
+    subheader: "My personal website",
+    description:
+      "My personal website for my portfolio. Will be working on making the website look good for different viewports as well as color palettes!",
+    demoLink: "",
+    githubLink: "https://github.com/GordonLei/gordonlei-react/tree/main",
+    images: [process.env.PUBLIC_URL + "/img/website.png"],
+  },
+  {
+    title: "Neco-Arc",
+    technology: ["Node.js", "Discord.js"],
+    subheader: "Personal Discord bot",
+    description:
+      'Personal project to learn how to make a Discord bot. It currently can search through the Anilist database via their API as well as allows users to "play" a game where they can test their knowledge about GRE vocabulary definitions. Will expand to my eventual project of a website dedicated for mechanical keyboard information (resources, group buy dates, etc).',
+    demoLink: "",
+    githubLink: "https://github.com/GordonLei/Neco-Arc",
+    images: [],
+  },
 ];
 
 export default WIP;
