d048dfae1ae9d9deb80db5b96c6d02d1ca443272
Gordon Lei <glei@andrew.cmu.edu>
2023-02-20 14:15:12 -0800

    remove unused imports

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,12 +1,3 @@
-import Navbar from "../components/Navbar";
-import Face from "../components/Face";
-import About from "../components/About";
-import Experience from "../components/Experience";
-import WIP from "../components/WIP";
-import Completed from "../components/Completed";
-import Resume from "../components/Resume";
-import Footer from "../components/Footer";
-import Skills from "../components/Skills";
 import Layout from "./layout";
 
 function Home() {
